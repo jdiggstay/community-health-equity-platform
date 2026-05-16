998b3cc8153b4c664b6f9069babfa4253219d3ed
JALISA DIGGS-TAYLOR <jalisadiggs-taylor@Mac.lan>
2026-05-16 18:54:29 -0500

    Add project metadata configuration

---
+++ config/project_info.py
@@ -0,0 +1,4 @@
+PROJECT_NAME = "Community Health Equity & Risk Intelligence Platform"
+VERSION = "0.1.0"
+AUTHOR = "Jalisa Diggs-Taylor"
+DESCRIPTION = "Using data to identify disparities and improve community health outcomes."
